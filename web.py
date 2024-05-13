35737c04398d70c1fa1bb63ecbc385164ef27e49
Akhil Robert <76222498+AkhilRobert@users.noreply.github.com>
2024-05-13 16:07:05 +0530

    Update web.py

--- web.py
+++ web.py
@@ -99,7 +99,7 @@ def load_data(t1ce, t2, flair, seg):
     world["final_image"] = final_image
     world["original"] = seg_data
 
-    return ((
+    return [(
         resized_img[:, :, world["current"], 2],
         [
             (final_image[:, :, world["current"]] == 1, "tumor core"),
@@ -113,8 +113,8 @@ def load_data(t1ce, t2, flair, seg):
                 (seg_data[:, :, world["current"]] == 2, "peritumoral edema"),
                 (seg_data[:, :, world["current"]] == 3, "enchancing tumor"),
             ],
-    )
-    )
+    )]
+    
 
 
 def render(slider, state):
@@ -125,24 +125,21 @@ def render(slider, state):
     if resized_img is None or final_image is None:
         gr.Warning("No predicted data is available")
 
-    return ((
-        resized_img[:, :, slider, 2],
+    return [(resized_img[:, :, slider, 2],
         [
             ((final_image[:, :, slider] == 1), "tumor core"),
             ((final_image[:, :, slider] == 2), "peritumoral edema"),
             ((final_image[:, :, slider] == 4), "enchancing tumor"),
         ],
-    ),
-
-        (
+    ), (
             resized_img[:, :, slider, 2],
             [
                 (seg_data[:, :, world["current"]] == 1, "tumor core"),
                 (seg_data[:, :, world["current"]] == 2, "peritumoral edema"),
                 (seg_data[:, :, world["current"]] == 3, "enchancing tumor"),
             ],
-        )
-    )
+        )]
+    
 
 
 with gr.Blocks() as demo:
